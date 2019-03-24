274b8ea4d13235e837fe7a65c7cc2225669d436f
Abisha Jesudas <abisha.jesudas@ocsbstudent.ca>
2019-03-24 18:07:17 -0400

    added stuff

--- TouchAndReactAbishaJ/main.lua
+++ TouchAndReactAbishaJ/main.lua
@@ -7,10 +7,10 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 -- set the background color
-display.setDefault ("background", 153/255, 255/255, 51/255)
+display.setDefault ("background", 178/255, 255/255, 102/255)
 
 -- create blue button, set its position and make it visible
-local blueButton = display.newImageRect("Images/ Fast Button Inactive@2x.png",198, 96)
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png",198, 96)
 blueButton.x = display.contentWidth/2
 blueButton.y = display.contentHeight/2
 blueButton.isVisible = true 
@@ -25,7 +25,7 @@ redButton.isVisible = false
 local textObject = display.newText ("Clicked!", 0, 0, nil, 50)
 textObject.x = display.contentWidth/2
 textObject.y = display.contentHeight/3
-textObject:setTextColor (0, 0, 0)
+textObject:setTextColor (0/255, 0/255, 255/255)
 textObject.isVisible = false
 
 local function BlueButtonListener(touch)
@@ -43,5 +43,5 @@ local function BlueButtonListener(touch)
 end
 
 -- add the respective listensers to each object
-blueButton:addedEventListener("touch", BlueButtonListener)
-redButton:addedEventListener("touch", BlueButtonListener)
+blueButton:addEventListener("touch", BlueButtonListener)
+redButton:addEventListener("touch", BlueButtonListener)
